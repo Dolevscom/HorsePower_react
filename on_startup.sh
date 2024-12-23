596457a552c71020e900085cbae8f5d2aa039301
Dolevscom <dolev.harel@mail.huji.ac.il>
2024-12-23 19:49:15 +0200

    open on start up

--- on_startup.sh
+++ on_startup.sh
@@ -3,14 +3,14 @@
 # Navigate to the React project directory
 cd /home/mada/Desktop/react_HorsePower/HorsePower_react/horsepower-react || exit
 
-# Serve the React build
-npx serve -s build &
+# Start the React app in the background
+npm start 
 
 # Navigate to the Python script directory (if different)
-cd /home/mada/Desktop/react_HorsePower/HorsePower_react/serial_to_websocket/serial_to_websocket.py || exit
+cd /home/mada/Desktop/react_HorsePower/HorsePower_react/serial_to_websocket || exit
 
 # Activate the virtual environment
 source /home/mada/Desktop/react_HorsePower/HorsePower_react/my_env/bin/activate
 
-# Run the Python script
-python serial_to_websockets.py
+# Start the Python WebSocket server
+python serial_to_websockets.py &
